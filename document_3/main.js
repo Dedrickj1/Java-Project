f7eb340dce0c8ff7e162b226eefe2416ea0588ff
dedrickj1 <dedrickjackson11@gmail.com>
2024-01-31 08:53:25 -0600

    Java Project

--- document_3/main.js
+++ document_3/main.js
@@ -11,21 +11,25 @@ const getData = async () => {
 
 // Create constants to hold dom elements
 const DOM_Elements = {
-    crypto_list :'.crypto-list'
-}
+    crypto_list :'.cryptocurrency-list'
+};
 
 // Create the crypto list html
-const create_list = (name ) => {
-    const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light"${name}</a>`
-    document.querySelector(DOM_Elements.crypto_list).insertAdjacentHTML('beforeend', html)
+const create_list = (symbol, name, max_supply) => {
+    const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light">${name} (${symbol} ${max_supply})</a>`;
+    document.querySelector(DOM_Elements.crypto_list).insertAdjacentHTML('beforeend', html);
 }
 
 // functions to load data and display HTML
 const load_data = async () => {
-    const crypto = await getData();
-    console.log(crypto);
-    console.log(typeof crypto);
-    crypto.name.forEach(crypto => create_list(crypto.symbol, crypto.name))
+    const cryptos = await getData();
+    console.log(cryptos);
+    console.log(typeof cryptos);
+
+ // Assuming `crypto` is an object where each key is a cryptocurrency symbol and its value is an object containing the name
+ Object.entries(cryptos).forEach(([symbol, cryptoInfo]) => {
+    create_list(symbol, cryptoInfo.name);
+});
 }
 
 const clear_data = () => {
